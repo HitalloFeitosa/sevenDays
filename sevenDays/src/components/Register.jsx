87ab7ccfcde5478612480fc59bc9a80893d2fb23
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2023-11-28 21:06:27 -0300

    feat: add form styles

--- sevenDays/src/components/Register.jsx
+++ sevenDays/src/components/Register.jsx
@@ -9,28 +9,28 @@ export default function Register() {
 
     return (
         <>
-            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center items-center'>
-                <h1 className="text-cyan-500 text-lg">Aluritter</h1>
-                <div>
+            <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col justify-center items-center w-72'>
+                <h1 className="text-cyan-500 text-lg mb-2.5">Aluritter</h1>
+                <div className="w-full mb-2">
                     <Controller
                         name="email"
                         control={control}
-                        render={({ field }) => <input {...field} className="border-solid border-2 rounded border-grey-500" type="email" placeholder="email@exemplo.com" />}
+                        render={({ field }) => <input {...field} className="border-solid border-2 rounded border-grey-500 h-8 w-full pl-1" type="email" placeholder="email@exemplo.com" />}
                     />
                 </div>
 
-                <div>
+                <div className="w-full mb-2.5">
                     <Controller
                         name="password"
                         control={control}
-                        render={({ field }) => <input {...field} className="border-solid border-2 rounded border-grey-500" type="password" placeholder="Senha" />}
+                        render={({ field }) => <input {...field} className="border-solid border-2 rounded border-grey-500 h-8 w-full pl-1" type="password" placeholder="Senha" />}
                     />
                 </div>
 
-                <button type="submit">Enviar</button>
+                <button type="submit" className="bg-green-500 h-8 w-full rounded text-white">Enviar</button>
 
-                <p>
-                    Já tem uma conta? <a href="#">Acesse agora!</a>
+                <p className="text-slate-400 text-sm mt-2.5">
+                    Já tem uma conta? <a href="#" className="text-sky-500">Acesse agora!</a>
                 </p>
             </form>
         </>
