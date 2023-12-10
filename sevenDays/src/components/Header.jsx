dc807fc155da560bea8c66745e2bb451eca26922
HitalloFeitosa <hitallo.feitosa@oficialfarma.com.br>
2023-12-10 11:02:46 -0300

    feat: change header structure

--- sevenDays/src/components/Header.jsx
+++ sevenDays/src/components/Header.jsx
@@ -2,15 +2,19 @@ export default function Header() {
     return (
         <>
             <header className="bg-white w-full">
-                <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+                <nav className="mx-auto flex max-w-7xl items-center justify-between p-2 lg:px-8" aria-label="Global">
                     <div className="flex lg:flex-1">
                         <a href="#" className="-m-1.5 p-1.5">
-                            <h1 className="text-cyan-500 text-xl mb-2.5 max-sm:text-3xl">Aluritter</h1>
+                            <h1 className="text-cyan-500 text-xl max-sm:text-3xl">Aluritter</h1>
                         </a>
                     </div>
                     <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-                        <p className="mr-3">user@gmail.com</p>
-                        <input type="button" className="bg-red-500 hover:bg-red-600 w-10 rounded focus:outline-none text-white text-sm cursor-pointer" value="sair" />
+                        <p className="text-slate-400 mr-3">user@gmail.com</p>
+                        <button className="bg-red-500 hover:bg-red-600 w-10 rounded focus:outline-none text-white text-sm">
+                            <span>
+                                sair
+                            </span>
+                        </button>
                     </div>
                 </nav>
             </header>
